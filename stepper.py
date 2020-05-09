2b53858552cd9b65485a72f099b2eabc0c0b6c46
Michael Karliner <mike@modern-indusrtry.com>
2020-05-09 17:38:55 +0100

    NEW: Socket server NEW: Move / stop setSpeed commands

--- stepper.py
+++ stepper.py
@@ -1,15 +1,25 @@
 from machine import Pin
 import time
 import uasyncio as asyncio
+import network
 
 # (c) IDWizard 2017
 # MIT License.
+
+async def connect():
+    station = network.WLAN(network.STA_IF)
+    station.active(True)
+    station.connect("mihome", "electr1c")
+    station.isconnected()
+    station.ifconfig()
+    #time.sleep(15)
 
 
 
 LOW = 0
 HIGH = 1
 FULL_ROTATION = int(4075.7728395061727 / 8) # http://www.jangeox.be/2013/10/stepper-motor-28byj-48_25.html
+DEFAULT_SPEED = 300
 
 HALF_STEP = [
     [LOW, LOW, LOW, HIGH],
@@ -29,42 +39,43 @@ FULL_STEP = [
  [HIGH, LOW, LOW, HIGH]
 ]
 
-class Command():
-    """Tell a stepper to move X many steps in direction"""
-    def __init__(self, stepper, steps, direction=1):
-        self.stepper = stepper
-        self.steps = steps
-        self.direction = direction
+# class Command():
+#     """Tell a stepper to move X many steps in direction"""
+#     def __init__(self, stepper, steps, direction=1):
+#         self.stepper = stepper
+#         self.steps = steps
+#         self.direction = direction
 
-class Driver():
-    """Drive a set of motors, each with their own commands"""
-
-    @staticmethod
-    async def run(commands):
-        """Takes a list of commands and interleaves their step calls"""
-        
-        # Work out total steps to take
-        max_steps = sum([c.steps for c in commands])
-
-        count = 0
-        while count != max_steps:
-            for command in commands:
-                # we want to interleave the commands
-                if command.steps > 0:
-                    await command.stepper.step(1, command.direction)
-                    command.steps -= 1
-                    count += 1
-                    print('Step')
+# class Driver():
+#     """Drive a set of motors, each with their own commands"""
+# 
+#     @staticmethod
+#     async def run(commands):
+#         """Takes a list of commands and interleaves their step calls"""
+#         
+#         # Work out total steps to take
+#         max_steps = sum([c.steps for c in commands])
+# 
+#         count = 0
+#         while count != max_steps:
+#             for command in commands:
+#                 # we want to interleave the commands
+#                 if command.steps > 0:
+#                     await command.stepper.step(1, command.direction)
+#                     command.steps -= 1
+#                     count += 1
+#                     print('Step')
         
 class Stepper():
-    def __init__(self, mode, pinno1, pinno2, pinno3, pinno4, delay=2):
+    def __init__(self, mode, pinno1, pinno2, pinno3, pinno4, speed=DEFAULT_SPEED):
         self.mode = mode
         self.pin1 = Pin(pinno1,Pin.OUT)
         self.pin2 = Pin(pinno2,Pin.OUT)
         self.pin3 = Pin(pinno3,Pin.OUT)
         self.pin4 = Pin(pinno4,Pin.OUT)
  
-        self.delay = delay  # Recommend 10+ for FULL_STEP, 1 is OK for HALF_STEP
+        self.delay = int(1000/speed)  # Recommend 10+ for FULL_STEP, 1 is OK for HALF_STEP
+                                 # speed is 1-1000 == 1000 - 1 millisecond delay
         
         # Initialize all to 0
         self.reset()
@@ -80,6 +91,20 @@ class Stepper():
                 await asyncio.sleep_ms(self.delay)
         self.reset()
         
+    async def move(self, direction=1, speed=DEFAULT_SPEED):
+        print('moving %d' % direction)
+        self.moving = True
+        while(self.moving):
+            await self.step(1, direction)
+            
+    def stop(self):
+        print('Stopping')
+        self.moving = False
+            
+        
+    def setSpeed(speed):
+        self.delay = 1000/speed
+        
     def reset(self):
         # Reset to 0, no holding, these are geared, you can't move them
         self.pin1.value(0) 
@@ -94,17 +119,45 @@ if __name__ == '__main__':
             tim = time.time()
             print("Mike %d " % tim)
             await asyncio.sleep(1)
+            
+    async def bounce():
+        while(True):
+            print('bounce forward')
+            asyncio.create_task(s1.move(1, 300))
+            print('wait')
+            await asyncio.sleep(10)
+            print('bounce stop')
+            s1.stop()
+            await asyncio.sleep(10)
+            print('bounce backwards')
+            asyncio.create_task(s1.move(-1, 300))
+            await asyncio.sleep(10)
+            print('bounce stop')
+            s1.stop()
+            await asyncio.sleep(10)
+            
+    async def servercb(reader, writer):
+        print('Connection made')
+        writer.write('Hello telnet'.encode())
+        await writer.drain()
+            
 
     loop = asyncio.get_event_loop()
-    s1 = Stepper(HALF_STEP, 12, 14, 27, 26, delay=5)    
+    s1 = Stepper(HALF_STEP, 12, 14, 27, 26, speed=300)    
     #s2 = Stepper(HALF_STEP, microbit.pin6, microbit.pin5, microbit.pin4, microbit.pin3, delay=5)   
     #s1.step(FULL_ROTATION)
     #s2.step(FULL_ROTATION)
     loop = asyncio.get_event_loop()
+    asyncio.run(connect())
     
-    runner = Driver()
-    loop.create_task(runner.run([Command(s1, FULL_ROTATION, 1)]))
+    server = asyncio.start_server(servercb, '0.0.0.0', 23, backlog=10)
+    
+    #runner = Driver()
+    #loop.create_task(runner.run([Command(s1, FULL_ROTATION, 1)]))
+    #loop.create_task(s1.move(1, 300))
+    loop.create_task(server)
     loop.create_task(hello())
+    loop.create_task(bounce())
     loop.run_forever()
     loop.close()
 #     asyncio.run(runner.run([Command(s1, FULL_ROTATION, 1)]))
